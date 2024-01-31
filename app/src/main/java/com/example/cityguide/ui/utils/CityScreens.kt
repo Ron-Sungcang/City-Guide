443fc43a0dbb5a0abc6c750e6e939ee0a12c2fc9
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-01-31 15:02:23 -0600

    Finished composable functions to display images for city and recommendations

--- app/src/main/java/com/example/cityguide/ui/utils/CityScreens.kt
+++ app/src/main/java/com/example/cityguide/ui/utils/CityScreens.kt
@@ -1,10 +1,15 @@
 package com.example.cityguide.ui.utils
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.example.cityguide.model.City
+import com.example.cityguide.model.Recommendation
 
 @Composable
 private fun CityListItem(
@@ -21,11 +26,26 @@ private fun CityListItem(
 @Composable
 private fun CityImageItem(city: City, modifier: Modifier = Modifier){
     Box(modifier = modifier){
-        Row {
-
-        }
+        Image(
+            painter = painterResource(id = city.imageRes),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillWidth
+        )
     }
 }
 
 @Composable
 private fun RecommendationListItem(){}
+
+@Composable
+private fun RecommendationImageItem(recommendation: Recommendation, modifier: Modifier = Modifier){
+    Box(modifier = modifier){
+        Image(
+            painter = painterResource(id =  recommendation.imageRes),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillWidth
+            )
+    }
+}
