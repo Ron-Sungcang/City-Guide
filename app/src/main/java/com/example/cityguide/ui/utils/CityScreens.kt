d12942e87039dac06b6e06c4b3133cf4367a27ea
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-02-03 18:02:10 -0600

    added gradle dependencies for the view model, started CityApp

--- app/src/main/java/com/example/cityguide/ui/utils/CityScreens.kt
+++ app/src/main/java/com/example/cityguide/ui/utils/CityScreens.kt
@@ -29,6 +29,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -38,12 +40,22 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.cityguide.R
 import com.example.cityguide.data.CityRepository
 import com.example.cityguide.model.City
 import com.example.cityguide.model.Recommendation
 import com.example.cityguide.ui.theme.CityGuideTheme
+
 import java.nio.file.WatchEvent
+
+@Composable
+fun CityApp(
+){
+    val viewModel: CityViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
+
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
